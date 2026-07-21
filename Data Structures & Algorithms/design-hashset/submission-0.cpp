c2f32fc2a1c27334edbda2a65892ceed45168d01
Anas Malvat <68893726+ANASMALVAT@users.noreply.github.com>
2026-07-21 16:43:56 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.cpp
@@ -0,0 +1,26 @@
+class MyHashSet {
+public:
+    vector<int> res;
+
+    MyHashSet() : res(1e7 + 1, 0) {} 
+
+    void add(int key) {
+        res[key] = 1;
+    }
+    
+    void remove(int key) {
+        res[key] = 0;
+    }
+    
+    bool contains(int key) {
+        return res[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
